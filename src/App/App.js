892eee2b57c7a2229622a610d30ce0689c4cd942
piebytwo <shivamsonidev@gmail.com>
2021-06-30 10:58:45 +0530

    adds new route for results page

--- src/App/App.js
+++ src/App/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Homepage from "pages/Homepage"
 import Rulepage from "pages/Rulepage"
 import Formpage from "pages/Formpage"
+import Resultspage from "pages/Resultspage"
 
 function App() {
   return (
     <BrowserRouter basename="/enterprise">
       <Switch>
+        <Route path="/results/:id" component={Resultspage} />
         <Route exact path="/" component={Homepage} />
         <Route exact path="/rules" component={Rulepage} />
         <Route exact path="/app" component={Formpage} />
