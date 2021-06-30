2b956297ff627e0da297f41e6d3a36de5cc6aee3
piebytwo <shivamsonidev@gmail.com>
2021-06-30 11:15:39 +0530

    resolves warning

--- src/pages/Formpage/Formpage.js
+++ src/pages/Formpage/Formpage.js
@@ -70,9 +70,9 @@ class Formpage extends React.Component {
     }
     getPath() {
         let path = '/results/'
-        this.state.steps_taken.map(step => {
+        let new_steps_taken = [...this.state.steps_taken]
+        new_steps_taken.map(step => function(){
             path = `${path}${step["option_selected"]}`
-            return;
         })
         return path
     }
