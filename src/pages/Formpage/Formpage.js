af47fb713cb649365b4d83035b7334b7aafc54ff
piebytwo <shivamsonidev@gmail.com>
2021-07-10 03:50:14 +0530

    adds rules popup on game screen

--- src/pages/Formpage/Formpage.js
+++ src/pages/Formpage/Formpage.js
@@ -3,6 +3,7 @@ import data from 'assets/data.json'
 import OptionButton from './OptionButton';
 import { NotEnoughPointsError, CompleteAllOptionsError } from './Errors'
 import { withRouter } from 'react-router-dom'
+import { HelpCircleSharp } from 'react-ionicons'
 
 class Formpage extends React.Component {
     state = {
@@ -15,7 +16,8 @@ class Formpage extends React.Component {
         }),
         show_error: false,
         not_enough_points_error: false,
-        select_all_error: false
+        select_all_error: false,
+        show_rules_popup: false
     }
     getCompletedSteps() {
         return this.state.steps_taken.reduce((a, v) => (v["answered"] ? a + 1 : a), 0)
@@ -103,6 +105,50 @@ class Formpage extends React.Component {
                     </div>
                 </div>
                 }
+                {this.state.show_rules_popup && 
+                <div className="flex inset-0 absolute z-50 w-screen h-screen">
+                    <div className="flex container rounded-lg mx-auto w-4/5 relative z-10 flex items-center">
+                        <div className="w-full flex flex-col items-center justify-between relative z-10">
+                            <div className="bg-white dark:bg-gray-800 rounded-lg overflow-hidden w-full relative">
+                                <div className="p-24 shadow-lg relative block">
+                                    <p className="text-center items-center text-sm mt-6">We've designed this exercise as a short game. As a user, you get a total of 50 points to invest in a combination of design choices. These can be spent on three types of options which have different weights.</p>
+                                    <div className="flex items-start items-stretch justify-between flex-row md:flex-row mt-6 w-full px-4">
+                                        <div className="flex flex-col w-full md:w-1/4 p-1">
+                                            <div className="p-4 rounded-lg bg-red-500 hover:bg-red-400 text-white">
+                                                <p className="flex flex-col text-center items-center text-sm">Red = 5 points</p>
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col w-full md:w-1/4 p-1">
+                                            <div className="p-4 rounded-lg bg-yellow-500 hover:bg-yellow-400 text-white">
+                                                <p className="flex flex-col text-center items-center text-sm">Yellow = 10 points</p>
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col w-full md:w-1/4 p-1">
+                                            <div className="p-4 rounded-lg bg-blue-500 hover:bg-blue-400 text-white">
+                                                <p className="flex flex-col text-center items-center text-sm">Blue = 15 points</p>
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col w-full md:w-1/4 p-1">
+                                            <div className="p-4 rounded-lg bg-purple-500 hover:bg-purple-400 text-white">
+                                                <p className="flex flex-col text-center items-center text-sm">Total of 50 points</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p className="text-center items-center text-sm mt-6">
+                                    The goal is to use these points to answer questions that relate to a few attributes of a "good steward". Keep in mind that your answers should represent your future aspirations as a steward. We've built this to illustrate both the trade-offs users have to make while building their stewarding entity and the finite investment possible. Use the flexibility to optimise for features that are important for your organization - but make sure to select an answer for each round or you will not be able to proceed to the next question
+                                    </p>
+                                    <p className="text-center items-center text-sm mt-6">
+                                    To modify your previous answer, use the back button
+                                    </p>
+                                    <div className="flex flex-col md:flex-row gap-4 justify-center relative max-w-lg md:max-w-4xl text-center items-center text-md">
+                                        <button onClick={()=>this.setState({ show_rules_popup: false })} className="flex bg-white hover:bg-black bg-blue-900 py-3 px-16 text-lg text-white font-bold uppercase rounded-sm mt-10 text-center rounded-md cursor-pointer">Resume</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                }
                 <div className="flex container rounded-lg mx-auto w-4/5 relative z-10 flex items-center">
                     <div className="w-full flex flex-col items-center justify-between relative z-10">
                         <div className="bg-white dark:bg-gray-800 rounded-lg overflow-hidden w-full relative">
@@ -120,7 +166,7 @@ class Formpage extends React.Component {
                                             <div>
                                                 {data.map((step, index) =>                                            
                                                 <p className={this.getState(index)} key={index}>
-                                                    <span className="mr-2 text-sm font-normal">{step.short}</span>
+                                                    <span className="mr-2 text-sm font-normal cursor-default">{step.short}</span>
                                                     {this.state.steps_taken[index]["answered"] &&
                                                     <span className="p-1 text-center ml-2 rounded-lg bg-blue-200 text-xs">
                                                         {this.getStepPoints(index)}
@@ -129,6 +175,9 @@ class Formpage extends React.Component {
                                                 )}
                                             </div>
                                         </nav>
+                                        <div className="ml-6 py-4">
+                                            <HelpCircleSharp color="#444" height="30px" width="30px" className="cursor-pointer" onClick={()=>this.setState({ show_rules_popup: true })} />
+                                        </div>
                                     </div>
                                 </div>
                                 
